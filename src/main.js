240fc6da9a93e9e2f88d0c472849956e814761f1
mohamedElbalky <mohammed4elbalky@gmail.com>
2024-07-04 03:16:57 +0300

    add footer and dark mode

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ const menuBtn = document.getElementById("menu_btn")
 const xBtn = document.getElementById("x_btn")
 const menuContent = document.getElementById("menu_content")
 
+const themeToggleBtn = document.querySelectorAll("#theme_toggle")
+
+const theme = localStorage.getItem("theme")
+
+theme && document.documentElement.classList.add(theme)
+
 
 
 menuBtn.addEventListener("click", () => {
@@ -16,4 +22,20 @@ xBtn.addEventListener("click", () => {
     menuContent.classList.add("hidden")
     menuBtn.classList.remove("hidden")
     xBtn.classList.add("hidden")
+})
+
+
+const handleThemeToggle = () => {
+    document.documentElement.classList.toggle("dark")
+    if (document.documentElement.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark")
+    } else {
+        localStorage.removeItem("theme")
+    }
+}
+
+themeToggleBtn.forEach(e => {
+    e.addEventListener("click", () => {
+        handleThemeToggle()
+    })
 })
